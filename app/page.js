ed764939ea47051431e826979a930329689ae696
unknown <youssef.ojeil@hotmail.com>
2024-10-06 13:03:03 -0400

    create hero section

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
-import Image from 'next/image';
+import { HeroSectionWithBeamsAndGrid } from "@/components/hero-section";
+import Image from "next/image";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <HeroSectionWithBeamsAndGrid />
+    </div>
+  );
 }
--- components/logo.jsx
+++ components/logo.jsx
@@ -1,6 +1,6 @@
-'use client';
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 
 export const Logo = () => {
   return (
@@ -8,12 +8,7 @@ export const Logo = () => {
       href="/"
       className="font-normal flex space-x-2 items-center text-sm mr-4  text-black px-2 py-1  relative z-20"
     >
-      <Image
-        src="/images/Leo's Coat White (Shirt-Front).jpg"
-        alt="logo"
-        width={30}
-        height={30}
-      />
+      <Image src="/images/leo-logo.png" alt="logo" width={60} height={60} />
       <span className="font-medium text-black dark:text-white">Leo's Coat</span>
     </Link>
   );
